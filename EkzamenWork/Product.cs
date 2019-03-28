1cf69cb2d1e32ef3cc619365f6f0a242343cf3c8
Azat <46015824+serikovskii@users.noreply.github.com>
2019-03-28 11:56:36 +0600

    экзамен

---
+++ EkzamenWork/Product.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EkzamenWork
+{
+    public class Product
+    {
+        public delegate void AddProduct(string message);
+        public event AddProduct Register;
+
+        public string Name { get; set; }
+        public int Quatnity { get; set; }
+        public int Price { get; set; }
+        public int CountProduct { get; set; } = 0;
+        public void ProductAdd(string name, int quantity, int price)
+        {
+            Name = name;
+            Quatnity = quantity;
+            Price = price;
+            RegisterProduct(name);
+            CountProduct++;
+        }
+        public Product()
+        {
+
+        }
+        
+        public void RegisterProduct(string name)
+        {
+            
+            if (Register != null)
+                Register($"Товар {name} добавлен в склад ");
+        }
+        public void ShowMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+        
+    }
+}
---
+++ EkzamenWork/Program.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EkzamenWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> product = new List<Product>();
+            Product pr = new Product();
+            pr.Register += pr.ShowMessage;
+            string name;
+            int price;
+            int quant;
+            JsonSerializer serializer = new JsonSerializer();
+            
+
+            do
+            {
+
+                name = Console.ReadLine();
+                price = Int32.Parse(Console.ReadLine());
+                quant = Int32.Parse(Console.ReadLine());
+                pr.ProductAdd(name, price, quant);
+                product.Add(new Product { Name = pr.Name, Price = pr.Quatnity, Quatnity = pr.Price });
+                
+            }
+            while (pr.CountProduct!=3);
+
+            
+            
+            using (StreamWriter sw = new StreamWriter("product.json"))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                serializer.Serialize(writer, product);
+            }
+        }
+    }
+}
